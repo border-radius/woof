e87f215aceecac32230ef65adee5719d2a28f1f6
border-radius <pharshmaster@gmail.com>
2016-03-12 16:13:43 +0300

    angular

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+var angular = require('angular');
+var ngRoute = require('angular-route');
+var ngResource = require('angular-resource');
+var ngSanitize = require('angular-sanitize');
+
+var app = angular.module('woof', [ngRoute, ngResource, ngSanitize]);
